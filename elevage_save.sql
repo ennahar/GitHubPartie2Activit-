1ce8282e4108ad3195da35dcfb216bc30e3e8e0c
ennahar <ennaharabdelhamid@gmail.com>
2018-11-25 13:19:45 +0100

    je me suis trompé de fichier, je viens d'envoyer le bon

--- elevage_save.sql
+++ elevage_save.sql
@@ -1,78 +1,239 @@
--- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
---
--- Host: localhost    Database: elevage
--- ------------------------------------------------------
--- Server version	8.0.13
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `animal`
---
-
-DROP TABLE IF EXISTS `animal`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `animal` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `espece` varchar(40) NOT NULL,
-  `sexe` char(1) DEFAULT NULL,
-  `date_naissance` datetime NOT NULL,
-  `nom` varchar(30) DEFAULT NULL,
-  `commentaire` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=244 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `animal`
---
-
-LOCK TABLES `animal` WRITE;
-/*!40000 ALTER TABLE `animal` DISABLE KEYS */;
-INSERT INTO `animal` VALUES (1,'chien','M','2010-04-05 13:43:00','Roucky',NULL),(2,'chat',NULL,'2010-03-24 02:23:00','Maya',NULL),(3,'chat','F','2011-09-13 15:02:00','Schtroumpfette',NULL),(4,'tortue','F','2001-08-03 05:12:00',NULL,NULL),(5,'chat',NULL,'2011-01-03 16:44:00','choupi','Ne sans oreille gauche'),(6,'tortue','F','2005-06-13 08:17:00','Bobosse','Carapace bizarre'),(7,'chien','F','2008-12-06 05:18:00','Caroline',NULL),(8,'chat','M','2008-09-11 15:38:00','Bagherra',NULL),(9,'tortue',NULL,'2010-08-23 05:18:00',NULL,NULL),(10,'chien','M','2010-07-21 15:41:00','Bobo',NULL),(11,'chien','F','2008-02-20 15:45:00','Canaille',NULL),(12,'chien','F','2009-05-26 08:54:00','Cali',NULL),(13,'chien','F','2007-04-24 12:54:00','Rouquine',NULL),(14,'chien','F','2009-05-26 08:56:00','Fila',NULL),(15,'chien','F','2008-02-20 15:47:00','Anya',NULL),(16,'chien','F','2009-05-26 08:50:00','Louya',NULL),(17,'chien','F','2008-03-10 13:45:00','Welva',NULL),(18,'chien','F','2007-04-24 12:59:00','Zira',NULL),(19,'chien','F','2009-05-26 09:02:00','Java',NULL),(20,'chien','M','2007-04-24 12:45:00','Balou',NULL),(21,'chien','M','2008-03-10 13:43:00','Pataud',NULL),(22,'chien','M','2007-04-24 12:42:00','Bouli',NULL),(23,'chien','M','2009-03-05 13:54:00','Zoulou',NULL),(24,'chien','M','2007-04-12 05:23:00','Cartouche',NULL),(25,'chien','M','2006-05-14 15:50:00','Zambo',NULL),(26,'chien','M','2006-05-14 15:48:00','Samba',NULL),(27,'chien','M','2008-03-10 13:40:00','Moka',NULL),(28,'chien','M','2006-05-14 15:40:00','Pilou',NULL),(29,'chat','M','2009-05-14 06:30:00','Fiero',NULL),(30,'chat','M','2007-03-12 12:05:00','Zonko',NULL),(31,'chat','M','2008-02-20 15:45:00','Filou',NULL),(32,'chat','M','2007-03-12 12:07:00','Farceur',NULL),(33,'chat','M','2006-05-19 16:17:00','Caribou',NULL),(34,'chat','M','2008-04-20 03:22:00','Capou',NULL),(35,'chat','M','2006-05-19 16:56:00','Raccou','Pas de queue depuis la naissance'),(36,'chat','M','2009-05-14 06:42:00','Boucan',NULL),(37,'chat','F','2006-05-19 16:06:00','Callune',NULL),(38,'chat','F','2009-05-14 06:45:00','Boule',NULL),(39,'chat','F','2008-04-20 03:26:00','Zara',NULL),(40,'chat','F','2007-03-12 12:00:00','Milla',NULL),(41,'chat','F','2006-05-19 15:59:00','Feta',NULL),(42,'chat','F','2008-04-20 03:20:00','Bilba','Sourde de l\'oreille droite '),(43,'chat','F','2007-03-12 11:54:00','Cracotte',NULL),(44,'chat','F','2006-05-19 16:16:00','Cawette',NULL),(45,'tortue','F','2007-04-01 18:17:00','Nikki',NULL),(46,'tortue','F','2009-03-24 08:23:00','Tortilla',NULL),(47,'tortue','F','2009-03-26 01:24:00','Scroupy',NULL),(48,'tortue','F','2006-03-15 14:56:00','Lulla',NULL),(49,'tortue','F','2008-03-15 12:02:00','Dana',NULL),(50,'tortue','F','2009-05-25 19:57:00','Cheli',NULL),(51,'tortue','F','2007-04-01 03:54:00','Chicaca',NULL),(52,'tortue','F','2006-03-15 14:26:00','Redbul','Insomniaque'),(53,'tortue','M','2007-04-02 01:45:00','Spoutnik',NULL),(54,'tortue','M','2008-03-16 08:20:00','Bubulle',NULL),(55,'tortue','M','2008-03-15 18:45:00','Relou','Surpoids'),(56,'tortue','M','2009-05-25 18:54:00','Bulbizard',NULL),(57,'perroquet','M','2007-03-04 19:36:00','Safran',NULL),(58,'perroquet','M','2008-02-20 02:50:00','Gingko',NULL),(59,'perroquet','M','2009-03-26 08:28:00','Bavard',NULL),(60,'perroquet','F','2009-03-26 07:55:00','Parlotte',NULL);
-/*!40000 ALTER TABLE `animal` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `test_tuto`
---
-
-DROP TABLE IF EXISTS `test_tuto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `test_tuto` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL DEFAULT 'blabla',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `test_tuto`
---
-
-LOCK TABLES `test_tuto` WRITE;
-/*!40000 ALTER TABLE `test_tuto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `test_tuto` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-11-17 18:13:41
+?USE [ProjetGestion]
+GO
+/****** Object:  Table [dbo].[HP]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HP](
+	[hp_id] [int] IDENTITY(1,1) NOT NULL,
+	[hp_date] [date] NOT NULL,
+	[hp_tj] [float] NOT NULL,
+	[hp_codeU] [varchar](5) NOT NULL,
+	[hp_codeP] [varchar](5) NULL,
+ CONSTRAINT [PK_HP] PRIMARY KEY CLUSTERED 
+(
+	[hp_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Manager]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Manager](
+	[m_id] [int] IDENTITY(1,1) NOT NULL,
+	[m_code] [varchar](5) NOT NULL,
+	[m_nom] [varchar](50) NOT NULL,
+	[m_prenom] [varchar](50) NOT NULL,
+	[m_email] [varchar](100) NULL,
+	[m_codeS] [varchar](5) NULL,
+ CONSTRAINT [PK_Manager] PRIMARY KEY CLUSTERED 
+(
+	[m_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Projet]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Projet](
+	[p_id] [int] IDENTITY(1,1) NOT NULL,
+	[p_code] [varchar](5) NOT NULL,
+	[p_desc] [varchar](100) NOT NULL,
+	[p_codeS] [varchar](5) NULL,
+ CONSTRAINT [PK_Projet] PRIMARY KEY CLUSTERED 
+(
+	[p_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Service]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Service](
+	[s_id] [int] IDENTITY(1,1) NOT NULL,
+	[s_code] [varchar](5) NOT NULL,
+	[s_desc] [varchar](100) NOT NULL,
+	[s_resp] [varchar](5) NOT NULL,
+ CONSTRAINT [PK_Service] PRIMARY KEY CLUSTERED 
+(
+	[s_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Utilisateur]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Utilisateur](
+	[u_id] [int] IDENTITY(1,1) NOT NULL,
+	[u_code] [varchar](5) NOT NULL,
+	[u_nom] [varchar](50) NOT NULL,
+	[u_prenom] [varchar](50) NOT NULL,
+	[u_email] [varchar](100) NULL,
+	[u_codeS] [varchar](5) NULL,
+	[u_codeM] [varchar](5) NULL,
+ CONSTRAINT [PK_Utilisateur] PRIMARY KEY CLUSTERED 
+(
+	[u_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[Identite]    Script Date: 25/11/2018 10:13:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[Identite]
+AS
+SELECT        u_code, u_nom + ' ' + u_prenom AS identite
+FROM            dbo.Utilisateur
+GO
+ALTER TABLE [dbo].[HP]  WITH CHECK ADD  CONSTRAINT [FK_HP_Projet] FOREIGN KEY([hp_codeP])
+REFERENCES [dbo].[Projet] ([p_code])
+GO
+ALTER TABLE [dbo].[HP] CHECK CONSTRAINT [FK_HP_Projet]
+GO
+ALTER TABLE [dbo].[HP]  WITH CHECK ADD  CONSTRAINT [FK_HP_Utilisateur] FOREIGN KEY([hp_codeU])
+REFERENCES [dbo].[Utilisateur] ([u_code])
+GO
+ALTER TABLE [dbo].[HP] CHECK CONSTRAINT [FK_HP_Utilisateur]
+GO
+ALTER TABLE [dbo].[Manager]  WITH CHECK ADD  CONSTRAINT [FK_Manager_Service] FOREIGN KEY([m_codeS])
+REFERENCES [dbo].[Service] ([s_code])
+GO
+ALTER TABLE [dbo].[Manager] CHECK CONSTRAINT [FK_Manager_Service]
+GO
+ALTER TABLE [dbo].[Projet]  WITH CHECK ADD  CONSTRAINT [FK_Projet_Projet] FOREIGN KEY([p_code])
+REFERENCES [dbo].[Projet] ([p_code])
+GO
+ALTER TABLE [dbo].[Projet] CHECK CONSTRAINT [FK_Projet_Projet]
+GO
+ALTER TABLE [dbo].[Projet]  WITH CHECK ADD  CONSTRAINT [FK_Projet_Service] FOREIGN KEY([p_codeS])
+REFERENCES [dbo].[Service] ([s_code])
+GO
+ALTER TABLE [dbo].[Projet] CHECK CONSTRAINT [FK_Projet_Service]
+GO
+ALTER TABLE [dbo].[Utilisateur]  WITH CHECK ADD  CONSTRAINT [FK_Utilisateur_Utilisateur] FOREIGN KEY([u_codeS])
+REFERENCES [dbo].[Manager] ([m_code])
+GO
+ALTER TABLE [dbo].[Utilisateur] CHECK CONSTRAINT [FK_Utilisateur_Utilisateur]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Utilisateur"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 136
+               Right = 247
+            End
+            DisplayFlags = 280
+            TopColumn = 1
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Identite'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'Identite'
+GO
